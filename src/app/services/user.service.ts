167e653891c32d657026153a77e471c073be91cb
itzcookiie <mayowaadeniyi@hotmail.com>
2020-01-18 05:56:08 +0000

    adding functioality for calling and displaying data

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,4 +6,25 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   constructor() { }
+
+  public userItems: UserItem[] = []
+
+  public async fetchData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/photos')
+    const responseData = await response.json()
+    const firstHundredItems = responseData.slice(0, 51)
+                                                        .map(item => this.userItems.push(item))
+    return responseData 
+  }
 }
+
+interface UserItem {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+
+
