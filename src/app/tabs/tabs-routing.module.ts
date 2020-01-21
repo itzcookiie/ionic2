4652fbd30ebf2a2365e76c2f0e4034302d5a9163
ChildOfGod2 <mayowaadeniyi@hotmail.com>
2020-01-21 20:16:19 +0000

    scaled user card images. Rerouted routes + added login validation

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
-import { UserDetailComponent } from '../user-detail/user-detail.component';
 
 
 const routes: Routes = [
